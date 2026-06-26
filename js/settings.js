e0ec6a6248a68b2d6f10f60cc7b529deb784dba8
Ferenc Lombai <ferenc.lombai@gmail.com>
2026-06-27 00:58:28 +0200

    Add level settings

---
+++ js/settings.js
@@ -0,0 +1,35 @@
+const Settings = {
+    start() {
+        App.showContent();
+        document.getElementById("content").innerHTML = `
+<div class="plant-card">
+    <h2>⚙️ Beállítások</h2>
+    <div class="settings-group">
+        <h3>Kvíz nehézsége</h3>
+        <label>
+            <input type="radio" name="difficulty" value="1" ${App.settings.difficulty == 1 ? "checked" : ""}>
+            🟢 Könnyű
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="difficulty" value="2" ${App.settings.difficulty == 2 ? "checked" : ""}>
+            🔴 Nehéz
+        </label>
+    </div>
+    <hr>
+    <hr>
+    <button id="backToMenuButton">
+        🏠 Menü
+    </button>
+</div>
+`;
+        document.querySelectorAll("input[name=difficulty]").forEach((radio) => {
+            radio.onchange = () => {
+                App.settings.difficulty = Number(radio.value);
+            };
+        });
+        document.getElementById("backToMenuButton").onclick = () => {
+            App.showMainMenu();
+        };
+    }
+};
